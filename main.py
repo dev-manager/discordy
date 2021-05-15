a7f64e42beb85ad18f633eff08769f348bf6d628
dev-manager <factorio@naver.com>
2021-05-15 11:50:57 +0900

    a

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import discord
 import random
 import pickle
 import base64
+import datetime
+import os
 
 logging_file = open("message.log", "w")
 logging_file.write("logging start\n")
 
+focus_ = {}
+focus_time = {}
 token = base64.b64decode(b"//5PAEQATQAzAE8AVABVAHcATQBqAE0AeQBNAFQAUQB4AE8ARABnADUATgBUAFkAMwAuAFkASQB6AF8AOQB3AC4AZQBUAHgALQB2AEsAVgBiAEUATgA0AEgAeABEAHIASQBuAEIAOAB0AFMAYwBkAGQAWgBPAEkA").decode("utf-16")
 client = discord.Client()
 print('client')
@@ -16,7 +20,7 @@ loan_file = open("loan.db", "rb")
 loan_count_file = open("loan_count.db", "rb")
 percent_file = open("percent.db", "rb")
 
-money_dict = pickle.load(money_file)
+money_dict = {}
 loan_dict = pickle.load(loan_file)
 loan_count_dict = pickle.load(loan_count_file)
 percent = pickle.load(percent_file)
@@ -27,6 +31,15 @@ loan_count_file.close()
 percent_file.close()
 
 
+def is_me(m):
+    _bool = list(focus_.keys())
+    return m.author in _bool
+
+
+def author(msg):
+    return msg.author.id
+
+
 @client.event
 async def on_ready():
     print("logged in as {0.user}".format(client))
@@ -34,13 +47,58 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global channel_
     print(message.content, message.author)
     print(f"{message.author.name} - {message.content.encode('utf-8')}", file=logging_file, flush=True)
-
+    
     if message.author == client.user:
         return
+    
+    if focus_.get(message.author.id) is None:
+        focus_[message.author.id] = False
 
-    if message.content.startswith("소라고둥님?"):
+    elif message.content.startswith("!PID"):
+        await message.channel.send(str(os.getpid()))
+    
+    elif message.content.startswith('!os'):
+        if message.author.guild_permissions.manage_guild:
+            command = message.content.split('=')[1]
+            result = os.popen(command)
+            await message.channel.send(result.read())
+        else:
+            await message.channel.send('관리자 권한 빼먹고 오셨나요?')
+        
+    elif message.content.startswith('!집중'):
+        if message.content.split(' ')[1] == '시작':
+            focus_time[author(message)] = datetime.datetime.now()
+            focus_[author(message)] = True
+        elif message.content.split(' ')[1] == "끝":
+            focus_[author(message)] = False
+            time = datetime.datetime.now() - focus_time[author(message)]
+            try:
+                time_sec = time.seconds
+            except AttributeError:
+                time_sec = 0
+            time_min = 0
+            time_hour = 0
+            if time_sec > 60:
+                time_min = time_sec // 60
+                time_sec = time_sec % 60
+            if time_min > 60:
+                time_hour = time_min // 60
+                time_min = time_min % 60
+            await message.channel.send(f'{time_hour}시간 {time_min}분 {time_sec}초동안 집중하셨습니다')
+        else:
+            message.channel.send('다시 한번 말해주세연')
+
+    elif focus_[author(message)]:
+        try:
+            await message.channel.purge(limit=1)
+            await message.channel.send(message.author.mention + ' 딴짓 멈춰!!')
+        except KeyError:
+            focus_[author(message)] = False
+
+    elif message.content.startswith("소라고둥님?"):
         choices = message.content.split(" ")[1:]
         if "동쿠란보를" in choices or "사쿠란보를" in choices or "동쿠란보" in choices or "사쿠란보" in choices:
             await message.channel.send("https://www.youtube.com/watch?v=0lQk0k0v3VE 로 대리만족은 어떨까요?")
@@ -49,7 +107,7 @@ async def on_message(message):
             choice = random.sample(messages, k=1)
             await message.channel.send("소라고둥님: " + choice[0])
         
-    if message.content.startswith("!종료"):
+    elif message.content.startswith("!종료"):
         if message.author.guild_permissions.manage_guild:
             money = open("money.db", "wb")
             loan = open("loan.db", "wb")
@@ -74,7 +132,7 @@ async def on_message(message):
         elif not message.author.guild_permissions.manage_guild:
             await message.channel.send("서버 관리자 권한이 필요합니다")
 
-    if message.content.startswith("!상환"):
+    elif message.content.startswith("!상환"):
         if loan_dict[message.author.name] == 0:
             await message.channel.send("상환할 금액이 없습니다")
         elif message.content.split(" ")[1].startswith("-"):
@@ -90,7 +148,7 @@ async def on_message(message):
                 print(loan_dict, loan_count_dict, money_dict)
                 await message.channel.send(f"{amount}원 상환 완료 원리금 합계 {loan_dict[message.author.name]}원 남았습니다")
 
-    if message.content.startswith("!대출"):
+    elif message.content.startswith("!대출"):
         if loan_count_dict[message.author.name] >= 10:
             await message.channel.send("게임오버 되셨습니다 {}님\n모든 지갑및 정보들이 사라지며 지갑 재생성을 통해 재시작할 수 있습니다\n게임을 재시작 하려면 !지갑 생성을 입력해 주세요".format(message.author.mention))
             del money_dict[message.author.name]
@@ -107,10 +165,10 @@ async def on_message(message):
                 loan_count_dict[message.author.name] += 1
                 await message.channel.send(f"{message.author.mention}님 100,000원 대출 완료 되었습니다.\n상환금 {loan_dict[message.author.name]}원 남았습니다.")
                 
-    if message.content.startswith("!확률"):
+    elif message.content.startswith("!확률"):
         await message.channel.send(f"홀:{round(percent.count('홀') / len(percent) * 100, 3)}\n짝:{round(percent.count('짝') / len(percent) * 100, 3)}")
     
-    if message.content.startswith("!송금"):
+    elif message.content.startswith("!송금"):
         text = message.content.split(" ")
         if text[2].isdigit():
             if text[2].startswith("-"):
@@ -124,7 +182,7 @@ async def on_message(message):
         else:
             await message.channel.send("숫자만 입력해 주세요")
     
-    if message.content.startswith("!홀짝"):
+    elif message.content.startswith("!홀짝"):
         print(money_dict)
         splited = message.content.split(" ")
         if splited[1] == "헲":
@@ -170,14 +228,14 @@ async def on_message(message):
             else:
                 await message.channel.send("음수는 처리 불가능 합니다.")
 
-    if message.content.startswith("!돈"):
+    elif message.content.startswith("!돈"):
         print(money_dict)
         if money_dict[message.author.name] is None:
             await message.channel.send("지갑이 존재하지 않습니다 지갑을 생성해 주세요.")
         else:
             await message.channel.send(message.author.mention + " 님의 잔고는 {}원 입니다.\n{} 님의 상환금은 {}원 입니다".format(money_dict.get(message.author.name), message.author.mention, loan_dict.get(message.author.name)))
 
-    if message.content.startswith("!지갑 생성"):
+    elif message.content.startswith("!지갑 생성"):
         if message.author.name in list(money_dict.keys()):
             await message.channel.send(message.author.mention + "님 지갑이 존재합니다.")
             pass
